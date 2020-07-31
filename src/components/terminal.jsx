3c7ecd1afdd596f5aaa6a04b5f787e3484e83087
Souvik <souvikde.ns@gmail.com>
2020-07-31 10:32:37 +0530

    added css file

--- src/components/terminal.jsx
+++ src/components/terminal.jsx
@@ -1,18 +1,19 @@
 import React, { useRef, useEffect } from 'react'
 import $ from 'jquery'
-import termial from 'jquery.terminal'
+import 'jquery.terminal'
+import 'jquery.terminal/css/jquery.terminal.css'
 
 const Terminal = props => {
     const mainRef = useRef(null)
     useEffect(() => {
-        $('#check').terminal({
+        $(mainRef.current).terminal({
             help: () => {
                 this.echo('\nList of commands available:');
             }
         })
     }, [])
 
-    return <div id="check" ref={mainRef} />
+    return <div ref={mainRef} />
 }
 
 
