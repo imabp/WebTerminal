43cc330cda08ef80ab40091641e1e10ae55ee129
Souvik <souvikde.ns@gmail.com>
2020-07-31 10:17:59 +0530

    terminal is working but having a error

--- src/components/terminal.jsx
+++ src/components/terminal.jsx
@@ -1,10 +1,18 @@
-import React,{useRef} from 'react'
+import React, { useRef, useEffect } from 'react'
 import $ from 'jquery'
+import termial from 'jquery.terminal'
 
 const Terminal = props => {
     const mainRef = useRef(null)
+    useEffect(() => {
+        $('#check').terminal({
+            help: () => {
+                this.echo('\nList of commands available:');
+            }
+        })
+    }, [])
 
-    return <div ref={mainRef} />
+    return <div id="check" ref={mainRef} />
 }
 
 
