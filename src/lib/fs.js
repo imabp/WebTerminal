504ebbf141d86d6cc7bdad79d9f8fe9c3dababba
Souvik <souvikde.ns@gmail.com>
2020-08-01 13:16:00 +0530

    created file system

--- src/lib/fs.js
+++ src/lib/fs.js
@@ -1,13 +1,26 @@
 /**
  * Here we will create a dummy file system 
  */
+import _ from 'lodash'
 class FileSystem{
     constructor(){
         this.path = '/'
+        this.fs = [
+            {
+                level: 0,
+                contents: ['Documents', 'Downloads']
+            }
+        ]
+        this.level = 0
     }
 
     getPath(){
-        return this.path
+        let data =  _.find(this.fs, {level: this.level})
+        return data.contents
+    }
+
+    changeDir(dir){
+        
     }
 
 }
