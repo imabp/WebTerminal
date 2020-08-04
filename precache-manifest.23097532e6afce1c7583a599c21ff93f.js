22416f86979a5fbba67e8a68bbd1d79266ed5598
imabp <abir.pal899@gmail.com>
2020-08-04 11:06:18 +0530

    Updates

---
+++ precache-manifest.23097532e6afce1c7583a599c21ff93f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "895d1cb7502ffa9682c33f7473851a0c",
+    "url": "/LearnBasicLinuxTerminal/index.html"
+  },
+  {
+    "revision": "b473e96e1224c7dc6c6b",
+    "url": "/LearnBasicLinuxTerminal/static/css/2.87812da9.chunk.css"
+  },
+  {
+    "revision": "5267b0271d9180c0a6fd",
+    "url": "/LearnBasicLinuxTerminal/static/css/main.e32b3ee5.chunk.css"
+  },
+  {
+    "revision": "b473e96e1224c7dc6c6b",
+    "url": "/LearnBasicLinuxTerminal/static/js/2.87271265.chunk.js"
+  },
+  {
+    "revision": "9ca33f50d702f2ac6bac64a70ddaeb11",
+    "url": "/LearnBasicLinuxTerminal/static/js/2.87271265.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5267b0271d9180c0a6fd",
+    "url": "/LearnBasicLinuxTerminal/static/js/main.78246388.chunk.js"
+  },
+  {
+    "revision": "fadacb8735d72c5cb59a",
+    "url": "/LearnBasicLinuxTerminal/static/js/runtime-main.f9f77e2a.js"
+  }
+]);
